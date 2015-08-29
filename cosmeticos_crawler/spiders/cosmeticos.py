6d3171ee3a228ff1748e4810234c60b2fcdf357f
Renan Lage <renalage@gmail.com>
2015-08-29 20:53:31 -0300

    Implement parse method to check all urls

--- cosmeticos_crawler/spiders/cosmeticos.py
+++ cosmeticos_crawler/spiders/cosmeticos.py
@@ -4,17 +4,26 @@ import scrapy
 from cosmeticos_crawler.items import ProductPageItem
 
 
-class CosmeticosSpider(scrapy.CrawlSpider):
-    name = "cosmeticos"
-    allowed_domains = ("http://www.epocacosmeticos.com.br")
-    start_urls = ('http://www.http://www.epocacosmeticos.com.br/')
+class CosmeticosSpider(scrapy.Spider):
+    name = 'cosmeticos'
+    allowed_domains = ['epocacosmeticos.com.br']
+    start_urls = [
+        'http://www.epocacosmeticos.com.br/'
+    ]
 
     def parse(self, response):
-        pass
+        # Identify a product page by end of url
+        # and call method to extract data from it
+        if response.url.endswith('/p'):
+            self.parse_item(response)
+
+        # If it's not a product page call parse for all other links
+        for href in response.xpath('//a/@href'):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse)
 
     def parse_item(self, response):
-        # Identify a product page by end of url
-        if response.url.endswith('/p')
+            # Set item attributes with data extracted from the page
             item = ProductPageItem()
             item['title'] = response.xpath('//head/title/text()').extract()
             item['name'] = response.xpath("//div[contains(concat(' ', normalize-space(@class), ' '), ' fn productName ')]/text()").extract()
