d346c21359d6924e59f673a217badd40446f87eb
Renan Lage <renalage@gmail.com>
2015-08-30 18:56:12 -0300

    Improve link extractors filter for better performance

--- cosmeticos_comparator.py
+++ cosmeticos_comparator.py
@@ -11,31 +11,36 @@ class CosmeticosSpiderComparator:
 
     def same_n_lines(self):
         # Count number of lines from first file
-        count = 0
+        n_lines1 = 0
         with open(self.filename1, 'rt') as f1:
             for line in f1:
-                count += 1
+                n_lines1 += 1
+
+        print "Number of lines in file " + self.filename1 + ": " + str(n_lines1)
 
         # Decrement number of lines in second file from counter
+        n_lines2 = 0
         with open(self.filename2, 'rt') as f2:
             for line in f2:
-                count -= 1
+                n_lines2 += 1
+
+        print "Number of lines in file " + self.filename2 + ": " + str(n_lines2)
 
         # If number of lines decremented is different than incremented return false
-        return count == 0
+        return n_lines1 == n_lines2
 
     def same_products(self):
         # Count the number of times a url appear in file 1 and 2
         with open(self.filename1, 'rt') as f1:
             reader = csv.reader(f1)
             for line in reader:
-                url = line[1]
+                url = line[0]
                 self.products_counter[url] += 1
 
         with open(self.filename2, 'rt') as f2:
             reader = csv.reader(f2)
             for line in reader:
-                url = line[1]
+                url = line[0]
                 self.products_counter[url] += 1
 
         # Check number of urls is always equal to 2
@@ -45,11 +50,30 @@ class CosmeticosSpiderComparator:
                 return False
         return True
 
+    def get_different_products(self):
+        products1 = []
+        with open(self.filename1, 'rt') as f1:
+            reader = csv.reader(f1)
+            for line in reader:
+                url = line[0]
+                products1.append(url)
+
+        products2 = []
+        with open(self.filename2, 'rt') as f2:
+            reader = csv.reader(f2)
+            for line in reader:
+                url = line[0]
+                products2.append(url)
+
+        for url in set(products1).symmetric_difference(set(products2)):
+            print url
+
 
 def main():
-    comparator = CosmeticosSpiderComparator('cosmeticos_sitemap.csv', 'cosmeticos_only_products.csv')
-    print comparator.same_n_lines() == True
-    print comparator.same_products() == True
+    comparator = CosmeticosSpiderComparator('cosmeticos_sitemap.csv', 'cosmeticos_deu_errado.csv')
+    print comparator.same_n_lines()
+    print comparator.same_products()
+    comparator.get_different_products()
 
 if __name__ == "__main__":
     main()
--- cosmeticos_crawler/spiders/cosmeticos.py
+++ cosmeticos_crawler/spiders/cosmeticos.py
@@ -12,22 +12,33 @@ class CosmeticosSpider(CrawlSpider):
     ]
 
     rules = (
-        # Extract links matching 'category.php' (but not matching 'subsection.php')
-        # and follow links from them (since no callback means follow=True by default).
-        Rule(LinkExtractor(deny=('/account', '/centralatendimento', '/giftList', '/site/', '/checkout', '/cart',
-                                 '-ate\d+', '-de\d+a\d+', 'de-\d+(,\d+)?-a-\d+', 'priceFrom', 'specificationFilter',
-                                 'outlet', 'brinde',
-                                ))),
-
         # Extract links matching product pages by the end of url
         # and call method to parse item info
-        Rule(LinkExtractor(allow=('./p$', )), callback='parse_item'),
+        Rule(LinkExtractor(allow=('./p$',), unique=False), callback='parse_item', follow=True),
+
+        # Only allow department pages to be visited and a few other special pages like offers and outlets
+        # Avoid visiting brand pages and visiting redundant pages
+        Rule(LinkExtractor(allow=('http://www.epocacosmeticos.com.br/perfumes.',
+                                  'http://www.epocacosmeticos.com.br/maquiagem.',
+                                  'http://www.epocacosmeticos.com.br/cabelos.',
+                                  'http://www.epocacosmeticos.com.br/dermocosmeticos.',
+                                  'http://www.epocacosmeticos.com.br/tratamentos.',
+                                  'http://www.epocacosmeticos.com.br/corpo-e-banho.',
+                                  'outlet', 'oferta', 'blackfriday', 'busca', 'brinde',),
+
+                           # Avoid visiting user account and user services related pages
+                           # Avoid discriminating products by price (redundant with other filters) and narrowing the
+                           # products views with specificationFilters
+                           deny=('account', 'centralatendimento', 'giftList', '/site/', '/checkout', '/cart',
+                                 'specificationFilter', 'priceFrom', '/.+http://www.epocacosmeticos.com.br/', './p$',
+                                 )), follow=True)
     )
 
     def parse_item(self, response):
         # and call method to extract data from it
         item = ProductPageItem()
         item['title'] = response.xpath('//head/title/text()').extract()
-        item['name'] = response.xpath("//div[contains(concat(' ', normalize-space(@class), ' '), ' fn productName ')]/text()").extract()
+        item['name'] = response.xpath(
+            "//div[contains(concat(' ', normalize-space(@class), ' '), ' fn productName ')]/text()").extract()
         item['url'] = response.url
         yield item
